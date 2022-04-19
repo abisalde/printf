fd12f7202b37ac11ac9ed56db46f78d982fdcd4e
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2022-04-19 23:01:35 +0000

    edit submit

--- ev_print_func.c
+++ ev_print_func.c
@@ -2,10 +2,12 @@
 
 /**
  * ev_print_func - returns the amount of identifiers.
- * @s: argument indentifier
+ * @s: argument indentifier.
  * @index: index of argument identifier.
  * Return: amount of identifiers.
  */
+
+
 int ev_print_func(const char *s, int index)
 {
 	print_t pr[] = {
