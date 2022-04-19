a77aece22326b9ca5506132a3cd18e8b65741abc
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2022-04-19 23:00:04 +0000

    printf.c

--- _printf.c
+++ _printf.c
@@ -2,8 +2,7 @@
 
 /**
  * _printf - formatted output conversion and print data.
- * @format: input string.
- *
+ * @format: input string character
  * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
